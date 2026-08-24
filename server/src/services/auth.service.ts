abf05aca5c0dac4440f4cbc1569d3649b71be8d5
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-08-24 06:26:53 +0530

    feat(auth): implement registration service

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,5 +1,30 @@
+import bcrypt from 'bcrypt'
 import {prisma} from "../config/prisma.js";
+import { RegisterInput } from '../validator/auth.validator.js';
 
-export const getUser = async()=>{
-    return prisma.user.findMany();
+export const registerUser = async (input:RegisterInput)=>{
+    const {fullName, username, email, password} = input
+
+    const existingUser = await prisma.user.findFirst({
+        where:{
+            OR: [{email}, {username}]
+        },
+    })
+
+    if(existingUser){
+        throw new Error("Email or username already exists")
+
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = await prisma.user.create({
+        data:{
+            fullName,
+            username,
+            email,
+            password:hashedPassword
+        },
+    });
+    return user;
 }
